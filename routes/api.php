5a3df1810af7000eb4eabe1adf867e1683d41bbe
johnchicangana <johnd.chicangana@gmail.com>
2024-11-30 17:37:43 -0500

    add swagger and configuration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+
+Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
